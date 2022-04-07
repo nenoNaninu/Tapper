221a008673920858034d1244bff5ab286078e899
nenoNaninu <neno@melonpan.pro>
2022-04-07 20:11:46 +0900

    update attribute

--- src/Tapper.Attributes/TranspilationSourceAttribute.cs
+++ src/Tapper.Attributes/TranspilationSourceAttribute.cs
@@ -1,8 +1,9 @@
 using System;
 
-namespace Tapper;
-
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum)]
-public class TranspilationSourceAttribute : Attribute
+namespace Tapper
 {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum)]
+    public class TranspilationSourceAttribute : Attribute
+    {
+    }
 }
