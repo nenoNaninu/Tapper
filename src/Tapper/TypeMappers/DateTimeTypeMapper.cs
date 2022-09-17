48f210f9cbdacdb8095156e4ed792df45b59fb26
nenoNaninu <neno@melonpan.pro>
2022-09-17 18:06:28 +0900

    support DateTimeOffset

--- src/Tapper/TypeMappers/DateTimeTypeMapper.cs
+++ src/Tapper/TypeMappers/DateTimeTypeMapper.cs
@@ -29,3 +29,31 @@ internal class DateTimeTypeMapper : ITypeMapper
         throw new InvalidOperationException($"DateTimeTypeMapper is not support {typeSymbol.ToDisplayString()}.");
     }
 }
+
+internal class DateTimeOffsetTypeMapper : ITypeMapper
+{
+    public ITypeSymbol Assign { get; }
+
+    public DateTimeOffsetTypeMapper(Compilation compilation)
+    {
+        Assign = compilation.GetTypeByMetadataName("System.DateTimeOffset")!;
+    }
+
+    public string MapTo(ITypeSymbol typeSymbol, ITranspilationOptions options)
+    {
+        if (SymbolEqualityComparer.Default.Equals(typeSymbol, Assign))
+        {
+            if (options.SerializerOption == SerializerOption.MessagePack)
+            {
+                return "[Date, number]";
+            }
+            else
+            {
+                return "(Date | string)";
+            }
+        }
+
+        throw new InvalidOperationException($"DateTimeOffsetTypeMapper is not support {typeSymbol.ToDisplayString()}.");
+    }
+}
+
