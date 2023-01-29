df361b37a9afa03ea4d07b8abed095e799148ab5
nenoNaninu <neno@melonpan.pro>
2023-01-30 01:36:33 +0900

    add tests

---
+++ tests/Tapper.Test.SourceTypes/MessagePackAttributes.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace MessagePack;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = true)]
+public class MessagePackObjectAttribute : Attribute
+{
+    public bool KeyAsPropertyName { get; private set; }
+
+    public MessagePackObjectAttribute(bool keyAsPropertyName = false)
+    {
+        this.KeyAsPropertyName = keyAsPropertyName;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+public class KeyAttribute : Attribute
+{
+    public int? IntKey { get; private set; }
+
+    public string StringKey { get; private set; }
+
+    public KeyAttribute(int x)
+    {
+        this.IntKey = x;
+    }
+
+    public KeyAttribute(string x)
+    {
+        this.StringKey = x;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+public class IgnoreMemberAttribute : Attribute
+{
+}
---
+++ tests/Tapper.Tests/AttributeAnnotatedClassesTest.cs
@@ -0,0 +1,186 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tapper.Test.SourceTypes;
+using Xunit.Abstractions;
+using Xunit;
+
+namespace Tapper.Tests;
+
+public class AttributeAnnotatedClassesTest
+{
+    private readonly ITestOutputHelper _output;
+
+    public AttributeAnnotatedClassesTest(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    // Issue:
+    //     options.SpecialSymbo.JsonPropertyNameAttribute / JsonIgnoreAttribute is always null.
+    //     In CompilationSingleton.cs, add MetadataReference.CreateFromFile(typeof(JsonPropertyNameAttribute).Assembly.Location)
+    //     but not effect.
+    //    [Fact]
+    //    public void Test0()
+    //    {
+    //        var compilation = CompilationSingleton.Compilation;
+
+    //        var options = new TranspilationOptions(
+    //            compilation,
+    //            SerializerOption.Json,
+    //            NamingStyle.None,
+    //            EnumStyle.Value,
+    //            NewLineOption.Lf,
+    //            4,
+    //            false,
+    //            true
+    //        );
+
+    //        var codeGenerator = new TypeScriptCodeGenerator(compilation, options);
+
+    //        var type = typeof(AttributeAnnotatedClass1);
+    //        var typeSymbol = compilation.GetTypeByMetadataName(type.FullName!)!;
+
+    //        var writer = new CodeWriter();
+
+    //        codeGenerator.AddType(typeSymbol, ref writer);
+
+    //        var code = writer.ToString();
+    //        var gt = @"/** Transpiled from Tapper.Test.SourceTypes.AttributeAnnotatedClass1 */
+    //export type AttributeAnnotatedClass1 = {
+    //    /** Transpiled from string */
+    //    name: string;
+    //}
+    //";
+
+    //        _output.WriteLine(code);
+    //        _output.WriteLine(gt);
+
+    //        Assert.Equal(gt, code, ignoreLineEndingDifferences: true);
+    //    }
+
+
+    //    [Fact]
+    //    public void Test1()
+    //    {
+    //        var compilation = CompilationSingleton.Compilation;
+
+    //        var options = new TranspilationOptions(
+    //            compilation,
+    //            SerializerOption.Json,
+    //            NamingStyle.None,
+    //            EnumStyle.Value,
+    //            NewLineOption.Lf,
+    //            4,
+    //            false,
+    //            true
+    //        );
+
+    //        var codeGenerator = new TypeScriptCodeGenerator(compilation, options);
+
+    //        var type = typeof(AttributeAnnotatedClass2);
+    //        var typeSymbol = compilation.GetTypeByMetadataName(type.FullName!)!;
+
+    //        var writer = new CodeWriter();
+
+    //        codeGenerator.AddType(typeSymbol, ref writer);
+
+    //        var code = writer.ToString();
+    //        var gt = @"/** Transpiled from Tapper.Test.SourceTypes.AttributeAnnotatedClass2 */
+    //export type AttributeAnnotatedClass2 = {
+    //    /** Transpiled from int */
+    //    Foo: number;
+    //    /** Transpiled from string */
+    //    name: string;
+    //}
+    //";
+
+    //        _output.WriteLine(code);
+    //        _output.WriteLine(gt);
+
+    //        Assert.Equal(gt, code, ignoreLineEndingDifferences: true);
+    //    }
+
+
+    [Fact]
+    public void Test2()
+    {
+        var compilation = CompilationSingleton.Compilation;
+
+        var options = new TranspilationOptions(
+            compilation,
+            SerializerOption.MessagePack,
+            NamingStyle.None,
+            EnumStyle.Value,
+            NewLineOption.Lf,
+            4,
+            false,
+            true
+        );
+
+        var codeGenerator = new TypeScriptCodeGenerator(compilation, options);
+
+        var type = typeof(AttributeAnnotatedClass3);
+        var typeSymbol = compilation.GetTypeByMetadataName(type.FullName!)!;
+
+        var writer = new CodeWriter();
+
+        codeGenerator.AddType(typeSymbol, ref writer);
+
+        var code = writer.ToString();
+        var gt = @"/** Transpiled from Tapper.Test.SourceTypes.AttributeAnnotatedClass3 */
+export type AttributeAnnotatedClass3 = {
+    /** Transpiled from string */
+    Name: string;
+}
+";
+
+        _output.WriteLine(code);
+        _output.WriteLine(gt);
+
+        Assert.Equal(gt, code, ignoreLineEndingDifferences: true);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var compilation = CompilationSingleton.Compilation;
+
+        var options = new TranspilationOptions(
+            compilation,
+            SerializerOption.MessagePack,
+            NamingStyle.None,
+            EnumStyle.Value,
+            NewLineOption.Lf,
+            4,
+            false,
+            true
+        );
+
+        var codeGenerator = new TypeScriptCodeGenerator(compilation, options);
+
+        var type = typeof(AttributeAnnotatedClass4);
+        var typeSymbol = compilation.GetTypeByMetadataName(type.FullName!)!;
+
+        var writer = new CodeWriter();
+
+        codeGenerator.AddType(typeSymbol, ref writer);
+
+        var code = writer.ToString();
+        var gt = @"/** Transpiled from Tapper.Test.SourceTypes.AttributeAnnotatedClass4 */
+export type AttributeAnnotatedClass4 = {
+    /** Transpiled from int */
+    Bar: number;
+    /** Transpiled from string */
+    Name: string;
+}
+";
+
+        _output.WriteLine(code);
+        _output.WriteLine(gt);
+
+        Assert.Equal(gt, code, ignoreLineEndingDifferences: true);
+    }
+}
