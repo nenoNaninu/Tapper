5378faf45f4a34a888b6f68e4a46e4c6b683b704
nenoNaninu <neno@melonpan.pro>
2023-02-14 23:58:22 +0900

    fix

--- tests/TypeScriptTest/src/fetch.string-enum.test.ts
+++ tests/TypeScriptTest/src/fetch.string-enum.test.ts
@@ -1,66 +1,66 @@
-// import { fetchType2, fetchType3, fetchType4, fetchType5, fetchType6, fetchType7 } from './fetch.string-enum'
-// import { MyEnum, Type2, Type3, Type4, Type5, Type6, Type7 } from './generated/json-string-enum/Tapper.Tests.Server.Models'
+import { fetchType2, fetchType3, fetchType4, fetchType5, fetchType6, fetchType7 } from './fetch.string-enum'
+import { MyEnum, Type2, Type3, Type4, Type5, Type6, Type7 } from './generated/json-string-enum/Tapper.Tests.Server.Models'
 
-// test('fetch1.json', async () => {
-//     const res = await fetchType2();
-//     const gt: Type2 =
-//     {
-//         dateTime: new Date('2022-02-06T00:00:00Z'),
-//         uri: "http://example.com/2",
-//     }
-//     expect(res).toEqual(gt);
-// });
+test('fetch1.json', async () => {
+    const res = await fetchType2();
+    const gt: Type2 =
+    {
+        dateTime: new Date('2022-02-06T00:00:00Z'),
+        uri: "http://example.com/2",
+    }
+    expect(res).toEqual(gt);
+});
 
-// test('fetch2.json', async () => {
-//     const res = await fetchType3();
-//     const gt: Type3 =
-//     {
-//         customTypeList: [
-//             { id: "3cc614b1-bbce-4677-afb2-9e8dc48c7677", name: "maya", value: 18 },
-//             { id: "ee1868fa-7ff2-4699-932a-bc9f331aea11", name: "kuro", value: 11 },
-//         ]
-//     }
-//     expect(res).toEqual(gt);
-// });
+test('fetch2.json', async () => {
+    const res = await fetchType3();
+    const gt: Type3 =
+    {
+        customTypeList: [
+            { id: "3cc614b1-bbce-4677-afb2-9e8dc48c7677", name: "maya", value: 18 },
+            { id: "ee1868fa-7ff2-4699-932a-bc9f331aea11", name: "kuro", value: 11 },
+        ]
+    }
+    expect(res).toEqual(gt);
+});
 
-// test('fetch4.json', async () => {
-//     const res = await fetchType4();
-//     const gt: Type4 =
-//     {
-//         myEnum: MyEnum.Four,
-//         value: 7
-//     }
-//     expect(res).toEqual(gt);
-// });
+test('fetch4.json', async () => {
+    const res = await fetchType4();
+    const gt: Type4 =
+    {
+        myEnum: MyEnum.Four,
+        value: 7
+    }
+    expect(res).toEqual(gt);
+});
 
 
-// test('fetch5.json', async () => {
-//     const res = await fetchType5();
-//     const gt: Type5 =
-//     {
-//         value: "S"
-//     }
-//     expect(res).toEqual(gt);
-// });
+test('fetch5.json', async () => {
+    const res = await fetchType5();
+    const gt: Type5 =
+    {
+        value: "S"
+    }
+    expect(res).toEqual(gt);
+});
 
-// test('fetch6.json', async () => {
-//     const res = await fetchType6();
+test('fetch6.json', async () => {
+    const res = await fetchType6();
 
-//     const gt: Type6 =
-//     {
-//         binary: Buffer.from([99, 7, 0]).toString("base64")
-//     }
+    const gt: Type6 =
+    {
+        binary: Buffer.from([99, 7, 0]).toString("base64")
+    }
 
-//     expect(res).toEqual(gt);
-// });
+    expect(res).toEqual(gt);
+});
 
-// test('fetch7.json', async () => {
-//     const res = await fetchType7();
+test('fetch7.json', async () => {
+    const res = await fetchType7();
 
-//     const gt: Type7 =
-//     {
-//         dateTimeOffset: new Date("2022-09-17T16:51:00.4600000+09:00")
-//     }
+    const gt: Type7 =
+    {
+        dateTimeOffset: new Date("2022-09-17T16:51:00.4600000+09:00")
+    }
 
-//     expect(res).toEqual(gt);
-// });
+    expect(res).toEqual(gt);
+});
