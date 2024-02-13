0aec6833dca4a6f3bac472f42259553c0f6ed8b9
nenoNaninu <neno@melonpan.pro>
2024-02-13 23:14:17 +0900

    add tests

--- tests/Tapper.Tests.Analyzer/AnalyzerTest.cs
+++ tests/Tapper.Tests.Analyzer/AnalyzerTest.cs
@@ -74,7 +74,7 @@ class NotSupport
     }
 
     [Fact]
-    public async Task Test_GenericType()
+    public async Task Test_GenericType0()
     {
         var code = """
 using Tapper;
@@ -96,5 +96,62 @@ public class NestedGenericClass<T1, T2>
 """;
 
         await VerifyAsync(code, "TAPP001", 15, 30, 15, 51);
+    }
+
+    [Fact]
+    public async Task Test_GenericType1()
+    {
+        var code = """
+using Tapper;
+using System;
+
+public class MyType
+{
+}
+
+[TranspilationSource]
+public class GenericClass1<T>
+{
+    public MyType MyType { get; set; }
+    public T GenericProperty { get; set; }
+}
+""";
+
+        await VerifyAsync(code, "TAPP001", 11, 19, 11, 25);
+    }
+
+    [Fact]
+    public async Task Test_GenericType2()
+    {
+        var code = """
+using Tapper;
+using System;
+
+[TranspilationSource]
+public class GenericClass1<T>
+{
+    public string StringProperty { get; set; }
+    public T GenericProperty { get; set; }
+}
+
+public class NestedGenericClass<T1, T2>
+{
+    public string StringProperty { get; set; }
+    public T1 GenericProperty { get; set; }
+    public GenericClass1<T1> GenericClass1Property { get; set; }
+}
+
+[TranspilationSource]
+public class DeeplyNestedGenericClass<A, B, C>
+{
+    public string StringProperty { get; set; }
+    public A GenericPropertyA { get; set; }
+    public B GenericPropertyB { get; set; }
+    public GenericClass1<A> GenericClass1Property { get; set; }
+    public NestedGenericClass<string, B> NestedGenericClassProperty { get; set; }
+}
+""";
+
+        await VerifyAsync(code, "TAPP001", 25, 42, 25, 68);
     }
 }
