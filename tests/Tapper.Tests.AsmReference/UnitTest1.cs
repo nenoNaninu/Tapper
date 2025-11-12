1d2e1f963d69fa8b507cb638144b9c7432f4dd72
nenoNaninu <neno@melonpan.pro>
2025-11-12 23:27:29 +0900

    bump packages

--- tests/Tapper.Tests.AsmReference/UnitTest1.cs
+++ tests/Tapper.Tests.AsmReference/UnitTest1.cs
@@ -30,7 +30,7 @@ public class UnitTest1
 
         foreach (var outputFilePath in outputFilePaths)
         {
-            var output = await File.ReadAllTextAsync(outputFilePath);
+            var output = await File.ReadAllTextAsync(outputFilePath, TestContext.Current.CancellationToken);
 
             if (outputFilePath.Contains("Tapper.Tests.AsmReference.SourceTypes1.ts"))
             {
